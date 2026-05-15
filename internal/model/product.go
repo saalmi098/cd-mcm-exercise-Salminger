aaa0d0436473c447f114347d8361bcb23a5e6c6e
Andreas Salminger <andisalminger@hotmail.com>
2026-05-15 17:03:44 +0200

    exercise 3, task 2: removed typo

--- internal/model/product.go
+++ internal/model/product.go
@@ -9,7 +9,6 @@ type Product struct {
 
 // Validate checks whether the product has valid fields.
 func (p *Product) Validate() bool {
-	// this recieves a pointer to the product
 	if p.Name == "" {
 		return false
 	}
